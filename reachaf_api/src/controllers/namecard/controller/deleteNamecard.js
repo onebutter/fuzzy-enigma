3f8d6f5a28cea9ff1da1de3a38f58ab313743301
Ken <cb3lee@uwaterloo.ca>
2018-03-04 23:13:06 -0500

    delete controller

--- reachaf_api/src/controllers/namecard/controller/deleteNamecard.js
+++ reachaf_api/src/controllers/namecard/controller/deleteNamecard.js
@@ -1,11 +1,41 @@
-import { Op } from 'sequelize';
 import models from 'models';
 
-const { User, Namecard } = models;
+const { Namecard } = models;
 
 export default async (req, res) => {
-  console.log(req);
-  res.json({
-    message: 'temp'
-  });
-}
+  const targetId = req.params.namecardId;
+  const targetNc = await Namecard.findById(targetId);
+  if (!targetNc) {
+    return res.status(404).json({
+      code: 'INVALID_ID',
+      message: `No Namecard exists with this ID: ${targetId}`
+    });
+  }
+
+  if (req.requestingUser.isAdmin()) {
+    return res.status(200).json(targetNc);
+  }
+
+  if (targetNc.UserId !== req.requestingUser.id) {
+    return res.status(403).json({
+      code: 'DELETE_NOT_ALLOWED',
+      message: "you are trying to delete someone else's namecard"
+    });
+  }
+
+  if (targetNc.privay === 'default') {
+    const publicNc = await Namecard.findOne({
+      where: {
+        privacy: 'public'
+      }
+    });
+    if (publicNc) {
+      publicNc.privacy = 'default';
+      await publicNc.save();
+    }
+  }
+
+  const result = await targetNc.destroy();
+
+  res.json(result);
+};
