57d3bed3215bebd9611261f4f06c3f0dd192f8a3
Ken <cb3lee@uwaterloo.ca>
2018-03-12 18:48:23 -0400

    retrieves access token with the given code

--- reachaf_api/src/controllers/extService/controller.js
+++ reachaf_api/src/controllers/extService/controller.js
@@ -1,4 +1,24 @@
+import request from 'request-promise';
+import config from 'config';
 
-export const getTokenForDiscord = (req, res) => {
-  console.log('hellowolrd');
+export const getTokenForDiscord = async (req, res) => {
+  const { discord } = config.extService;
+  const data = {
+    client_id: discord.clientID,
+    client_secret: discord.clientSecret,
+    grant_type: 'authorization_code',
+    code: req.body.code,
+    redirect_uri: req.body.redirectUrl
+  };
+
+  const options = {
+    method: 'POST',
+    url: discord.tokenURL,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    form: data
+  };
+
+  const xx = await request(options);
+
+  console.log(xx)
 };
